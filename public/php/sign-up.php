5cbe085de74b386a5ed6be8f3701f7f4e27b2014
Eracres <122403544+Eracres@users.noreply.github.com>
2023-10-21 14:12:48 +0200

    Se ha creado un sign-up.php linkeado con sign-up.html para poder volcar la información a nuestra base de datos create.sql

--- public/php/sign-up.php
+++ public/php/sign-up.php
@@ -1,4 +1,34 @@
 <?php
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "tu_usuario";
+$password = "tu_contraseña";
+$dbname = "create.sql";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Error en la conexión a la base de datos: " . $conn->connect_error);
+}
+
+// Recopila los datos del formulario
+$name = $_POST['name'];
+$NIF = $_POST['nif'];
+$email = $_POST['email'];
+$adress = $_POST['location'];
+$country = $_POST['country'];
+$phone = $_POST['phone'];
+
+
+// Inserta los datos en la base de datos
+$sql = "INSERT INTO COMPANY (CIF, NAME, COUNTRY, ADDRESS, PHONE, EMAIL) VALUES ('$NIF', $name, $country, $adress, $phone, '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Datos insertados con éxito.";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
 
