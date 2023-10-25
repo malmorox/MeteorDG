b7a4fb0ad2226088e9cf196c8f1dc0ed27b2cb91
Eracres <122403544+Eracres@users.noreply.github.com>
2023-10-25 20:27:18 +0200

    Creados los siguientes SQL:
    -Consultas (vacio)
    -Inserts (en proceso)

---
+++ bin/db/inserts.sql
@@ -0,0 +1,44 @@
+
+/*Empresas existentes en nuestra página*/
+/*BMW*/
+INSERT INTO COMPANY VALUES ("A28713642", "BMW SAU", "GERMANY", "MUNCHEN", "Bremer Str. 6 80807", "+49893820","rclientes@bmw.es");
+/*Ford*/
+INSERT INTO COMPANY VALUES ("", "FORD MOTOR COMPANY", "", "", "", "");
+/*Ubisoft*/
+INSERT INTO COMPANY VALUES ("", "UBISOFT", "", "", "", "");
+/*Nintendo*/
+INSERT INTO COMPANY VALUES ("", "NINTENDO", "", "", "", "");
+/*NetFlix*/
+INSERT INTO COMPANY VALUES ("", "NETFLIX", "", "", "", "");
+/*Crunchyroll*/
+INSERT INTO COMPANY VALUES ("", "CRUNCHYROLL", "", "", "", "");
+/*Warner Bros*/
+INSERT INTO COMPANY VALUES ("", "WARNER BROS COMPANY", "", "", "", "");
+/*Universal Studio*/
+INSERT INTO COMPANY VALUES ("", "UNIVERSAL", "", "", "", "");
+/*Microsoft*/
+INSERT INTO COMPANY VALUES ("", "MICROSOFT", "", "", "", "");
+/*Intel*/
+INSERT INTO COMPANY VALUES ("", "INTEL", "", "", "", "");
+/*Spanair*/
+INSERT INTO COMPANY VALUES ("", "SPANAIR", "", "", "", "");
+/*Air France*/
+INSERT INTO COMPANY VALUES ("", "AIR FRANCE", "", "", "", "");
+/*BBVA*/
+INSERT INTO COMPANY VALUES ("", "BBVA", "", "", "", "");
+/*Bank of America*/
+INSERT INTO COMPANY VALUES ("", "BANK OF AMERICA", "", "", "", "");
+/*Hilton*/
+INSERT INTO COMPANY VALUES ("", "HILTON", "", "", "", "");
+/*Royal Caribian*/
+INSERT INTO COMPANY VALUES ("", "ROYAL CAREBIAN", "", "", "", "");
+/*MC Donalds*/
+INSERT INTO COMPANY VALUES ("", "MC DONALDS", "", "", "", "");
+/*KFC*/
+INSERT INTO COMPANY VALUES ("", "", "", "", "", "");
+/*Studio Ghibli*/
+INSERT INTO COMPANY VALUES ("", "", "", "", "", "");
+/*New Line Cinema*/
+INSERT INTO COMPANY VALUES ("", "", "", "", "", "");
+
+/*Empresas a introducir manualmente*/
